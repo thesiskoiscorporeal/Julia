7fc9f0b439a429ac1f2f3f0fa956a9fb4200dfe2
Drish <dcdrish@gmail.com>
2021-03-21 22:40:09 +0800

    done with HW4! (may have some mistakes though? time to peer grade...)

--- Homework/hw4_debugging.jl
+++ Homework/hw4_debugging.jl
@@ -1,4 +1,4 @@
-## returns lowest possible sum energy at pixel (i, j), and the column to jump to in row i+1.
+``## returns lowest possible sum energy at pixel (i, j), and the column to jump to in row i+1.
 
 function least_energy(energies, i, j)
 	m, n = size(energies)
@@ -33,3 +33,25 @@ grant_example =
 0.9  0.0  0.0  0.5  0.9  0.4
 0.2  0.4  0.0  0.2  0.4  0.5
 0.2  0.4  0.2  0.5  0.3  0.0]
+
+
+function least_energy_matrix(energies)
+	result = copy(energies)
+	m,n = size(energies)
+
+	for row in m-1:-1:1 #iterate through rows, bottom up
+
+		#edge cases #result[i,j] is energies[i,j] plus the lowest of the 3 below
+		result[row, 1] = energies[row, 1] + min([result[row+1, 1], result[row+1, 2]]...)
+		result[row, n] = energies[row, n] + min([result[row+1, n], result[row+1, n-1]]...)
+
+		#iterate through cols 2 to n-1
+		for col in 2:n-1
+			result[row, col] = energies[row, col] + min([result[row, i] for i in col-1:col+1]...)
+		end
+	end
+
+	return result
+end
+
+grant_example
